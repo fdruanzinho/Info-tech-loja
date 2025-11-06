ab67725b116f19caaa2986afe782c3ac9d01b9a8
Ruan Castilho <ruanlucasdecastilho05@gmail.com>
2025-11-05 23:53:56 -0300

    Atualiza Produto.jsx adicionando uso de estado (useState) e botão de curtidas

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './Produto.css'
 
 export default function Produto({ nome, preco, descricao }) {
+  const [likes, setLikes] = useState(0)
+
   return (
     <div className="produto">
       <h2>{nome}</h2>
       <p>{descricao}</p>
       <span className="preco">R$ {preco}</span>
+      <div>
+        <button onClick={() => setLikes(likes + 1)}>
+          ❤️ Curtir ({likes})
+        </button>
+      </div>
     </div>
   )
 }
+
+
